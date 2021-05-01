32d5a54662d144d1f4b002d7b972d4968a799485
n1ckjansens <n1ckjansens@yandex.ru>
2021-05-02 01:23:54 +0300

    .travis.yml added

--- frontend/.babelrc.js
+++ frontend/.babelrc.js
@@ -1,14 +1,14 @@
 const presets = [
-    "@babel/preset-env",
-    "@babel/preset-typescript",
-    "@babel/preset-react"
+	"@babel/preset-env",
+	"@babel/preset-typescript",
+	"@babel/preset-react"
 ];
 
 const plugins = [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread",
-    "@babel/plugin-transform-runtime",
-    "react-refresh/babel"
+	"@babel/proposal-class-properties",
+	"@babel/proposal-object-rest-spread",
+	"@babel/plugin-transform-runtime",
+	"react-refresh/babel"
 ];
 
-module.exports({ presets, plugins });
+module.exports = { presets, plugins };
---
+++ scripts/run-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BACKEND_DIRECTORY="./backend";
+FRONTEND_DIRECTORY="./frontend";
+
+if [ -d "$FRONTEND_DIRECTORY" ]; then
+    cd $FRONTEND_DIRECTORY
+    npm install || { echo 'Installling dependencies failed' ; exit 1; }
+    npm run lint || { echo 'Linters failed' ; exit 1; }
+    npm run build || { echo 'Production build failed' ; exit 1; }
+    npm run test || { echo 'Tests failed' ; exit 1; }
+fi
+
+if [ -d "$BACKEND_DIRECTORY" ]; then
+    cd ../
+    cd $BACKEND_DIRECTORY
+    npm install || { echo 'Installling dependencies failed' ; exit 1; }
+    npm run lint || { echo 'Linters failed' ; exit 1; }
+    npm run build || { echo 'Production build failed' ; exit 1; }
+    npm run test || { echo 'Tests failed' ; exit 1; }
+fi
